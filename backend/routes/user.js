943924a83a9f213d6ebf9546ce05ea0969e0d83d
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-11-27 17:57:41 +0530

    code modified

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -7,23 +7,4 @@ router.post("/register", register);
 
 router.post("/login", login);
 
-const restrictAdminAccess = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.user.id); // Assuming req.user contains logged-in user info
-    if (user.isAdmin) {
-      return res
-        .status(403)
-        .json({ message: "Admins cannot access user panel." });
-    }
-    next();
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-};
-
-// Example route for user panel
-router.get("/user-dashboard", restrictAdminAccess, (req, res) => {
-  res.status(200).json({ message: "Welcome to the user dashboard." });
-});
-
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -3,8 +3,7 @@ const app = express();
 const dotenv = require("dotenv");
 const connect = require("./config/data");
 const userRoute = require("./routes/user");
-const blogRoute = require("./routes/blog");
-const adminRoute = require("./routes/admin");
+
 dotenv.config();
 
 connect();
@@ -12,8 +11,6 @@ const port = process.env.port;
 
 app.use(express.json());
 app.use("/api/user", userRoute);
-app.use("/api/blog", blogRoute);
-app.use("/api/admin", adminRoute);
 
 app.listen(port, () => {
   console.log(`server is listning on port ${port}`);
