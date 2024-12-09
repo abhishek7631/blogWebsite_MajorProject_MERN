10c51ec7dbf7d1625c44aebece542b019ec44cdc
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-09 20:11:42 +0530

    modify blog model

--- backend/models/blog.js
+++ backend/models/blog.js
@@ -1,18 +1,28 @@
 const mongoose = require("mongoose");
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true },
-  content: { type: String, required: true },
-  author: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
-  comments: [
-    {
-      user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-      comment: { type: String, required: true },
+const blogSchema = new mongoose.Schema(
+  {
+    title: { type: String, required: true },
+    content: { type: String, required: true },
+    author: {
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      name: { type: String, required: true },
     },
-  ],
-  createdAt: { type: Date, default: Date.now },
-});
+    images: [String],
+    likes: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    comments: [
+      {
+        user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        comment: { type: String, required: true },
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 const Blog = mongoose.model("Blog", blogSchema);
 
