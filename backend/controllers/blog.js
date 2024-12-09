937faad9fe88a106304e84e25739664469ae2b26
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-12-09 22:44:44 +0530

    create blog route and controller

--- backend/controllers/blog.js
+++ backend/controllers/blog.js
@@ -11,3 +11,23 @@ exports.getAllBlog = async (req, res) => {
     res.status(400).json({ message: error });
   }
 };
+
+exports.getSpecificBlog = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const blog = await Blog.findById(id)
+      .populate("author", "name-_id")
+      .populate("comments.user", "name");
+    res.status(200).json(blog);
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
+
+exports.create = async (req, res) => {
+  const { title, content } = req.body;
+  try {
+  } catch (error) {
+    res.status(400).json({ message: error });
+  }
+};
